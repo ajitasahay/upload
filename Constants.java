8483b51983f9a49e23b14b6c91ab5f4ae0e81a7d
ajitasahay <33894373+ajitasahay@users.noreply.github.com>
2017-11-23 12:44:11 +0530

    Delete Constants.java

--- Constants.java
+++
@@ -1,9 +0,0 @@
-package com.craftsvilla.generic;
-
-public interface Constants {
-	
-	String url = "http://www.craftsvilla.com";
-	String userName = "ajitasahay@gmail.com";	
-    String password = "ajita1607!!";			
-    String browser = "chrome";
-}
